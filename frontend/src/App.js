1f41795defe4f2c85b09400045632b6913c0e374
Tim Klimpel <tim.klimpel@gmail.com>
2022-10-01 12:55:58 -0400

    clear form on submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,8 +11,8 @@ const App = () => {
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    //console.log(e.target[0].value);
-    console.log(word);
+    // console.log(e.target[0].value);
+    // console.log(word);
     fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
       .then((res) => res.json())
       .then((data) => {
@@ -21,6 +21,8 @@ const App = () => {
       .catch((err) => {
         console.log(err);
       })
+
+    setWord(''); //reseting form feild after enter
   }
 
   // console.log(UNSPLASH_KEY);
