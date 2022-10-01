565a0cdac935fdc0f82f82dc8cb6fe5c41e5035c
Tim Klimpel <tim.klimpel@gmail.com>
2022-10-01 12:29:47 -0400

    unsplash api random used and tested

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
 import Search from './components/search';
 
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_PUB_KEY;
+const UNSPLASH_BASE_URL = `https://api.unsplash.com/`;
 
 const App = () => {
   const [word, setWord] = useState('');
@@ -12,9 +13,17 @@ const App = () => {
     e.preventDefault();
     //console.log(e.target[0].value);
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 
-  //console.log(word);
+  // console.log(UNSPLASH_KEY);
 
   return (
     <div>
