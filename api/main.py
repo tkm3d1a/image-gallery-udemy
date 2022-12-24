38fe83b2a0792769baa8b04a255bad091f6f7131
Tim Klimpel <tim.klimpel@gmail.com>
2022-12-24 11:45:49 -0500

    updated linting and autoformat settings for python

--- api/main.py
+++ api/main.py
@@ -7,12 +7,14 @@ from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_URL="https://api.unsplash.com/photos/random/"
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
-DEBUG=bool(os.environ.get("DEBUG", True))
+UNSPLASH_URL = "https://api.unsplash.com/photos/random/"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError("Please create/add the UnsplashAPI key to the .env.local file in the root directory")
+    raise EnvironmentError(
+        "Please create/add the UnsplashAPI key to the .env.local file in the root directory"
+    )
 
 app = Flask(__name__)
 CORS(app)
@@ -22,24 +24,21 @@ app.config["DEBUG"] = DEBUG
 
 @app.route("/")
 def home():
-    return("You are on the home page, check the url")
+    return "You are on the home page, check the url"
+
 
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
 
-    headers = {
-        "Accept-Version": "v1",
-        "Authorization": "Client-ID " + UNSPLASH_KEY
-    }
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
 
-    params = {
-        "query": word
-    }
+    params = {"query": word}
 
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
 
+
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=5050)
